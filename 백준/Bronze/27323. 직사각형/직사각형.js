6d67befe44e042a9b556bfd66d42161d4de25e4c
sht02048 <131152690+sht02048@users.noreply.github.com>
2023-10-05 12:59:09 +0900

    [Bronze V] Title: 직사각형, Time: 120 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.js
@@ -0,0 +1,9 @@
+const { log } = require('console');
+let fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.text';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const x = Number(input[0]);
+const y = Number(input[1]);
+
+console.log(x * y);
