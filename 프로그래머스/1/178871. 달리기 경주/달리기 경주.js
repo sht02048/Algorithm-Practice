6ccf91c52bbd0f43498d168e26084093a1834bbc
Minsug Chae <sht02048@gmail.com>
2024-04-15 15:22:02 +0900

    [level 1] Title: 달리기 경주, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,20 +1,23 @@
 function solution(players, callings) {
-    
-    const ordersByIndex = new Map();
-    const ordersByName = new Map();
-    
-    players.forEach((player, index) => ordersByIndex.set(index, player));
-    players.forEach((player, index) => ordersByName.set(player, index));
+    // Create a map to store each player's index for quick access.
+    const playerIndexMap = new Map(players.map((player, index) => [player, index]));
 
-    for (let i = 0; i < callings.length; i += 1) {
-        const playerIndex = ordersByName.get(callings[i]);
-        const frontPlayer = ordersByIndex.get(playerIndex - 1);
-        
-        ordersByIndex.set(playerIndex, frontPlayer);
-        ordersByIndex.set(playerIndex - 1, callings[i]);
-        ordersByName.set(frontPlayer, playerIndex);
-        ordersByName.set(callings[i], playerIndex - 1);
-    }
-    
-    return ([...ordersByIndex.values()]);
+    // Process each calling by moving the called player ahead in the list.
+    callings.forEach(calling => {
+        const currentIndex = playerIndexMap.get(calling);
+        if (currentIndex > 0) { // Ensure there is a player in front to swap with
+            const previousIndex = currentIndex - 1;
+            const previousPlayer = players[previousIndex];
+
+            // Swap players in the array
+            players[currentIndex] = previousPlayer;
+            players[previousIndex] = calling;
+
+            // Update the indices in the map
+            playerIndexMap.set(calling, previousIndex);
+            playerIndexMap.set(previousPlayer, currentIndex);
+        }
+    });
+
+    return players;
 }
