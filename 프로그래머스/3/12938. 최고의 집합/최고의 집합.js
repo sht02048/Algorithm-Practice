bbe9631dee781383f8f2b053d7bfe6e8e86c6d79
Minsug Chae <sht02048@gmail.com>
2024-06-10 13:10:34 +0900

    [level 3] Title: 최고의 집합, Time: 0.05 ms, Memory: 32.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
@@ -0,0 +1,13 @@
+function solution(n, s) {
+    if(n > s) return [-1];
+
+    const ans = [];
+
+    for (i = n; i > 0; i--) {
+        const avg = Math.floor(s/i);
+        ans.push(avg);
+        s -= avg
+    }
+
+    return ans;
+}
